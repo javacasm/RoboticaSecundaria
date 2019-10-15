9d7c62b2a1b6711bc8501deeaea6bddcf73e4254
Javacasm <javacasm@gmail.com>
2019-10-15 10:27:40 +0200

    update

--- Codigo/arduinoblocks_173260.ino
+++ Codigo/arduinoblocks_173260.ino
@@ -7,24 +7,23 @@ Servo servo_8;
 
 Servo servo_7;
 
-void setup()
-{
-  pinMode(A4, INPUT);
-pinMode(A5, INPUT);
-servo_8.attach(8);
+void setup(){
+    pinMode(A4, INPUT);
+    pinMode(A5, INPUT);
 
-servo_7.attach(7);
-
+    servo_8.attach(8);
+    servo_7.attach(7);
 }
 
 
-void loop()
-{
+void loop(){
 
     AnguloX = map(analogRead(A4), 0,1023,0,180);
     AnguloY = map(analogRead(A5), 0,1023,0,180);
+    
     servo_8.write(AnguloX);
     delay(100);
+    
     servo_7.write(AnguloY);
     delay(100);
 
